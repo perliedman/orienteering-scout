53014671683f50e0cf341bd49087e992bbf176d9
Per Liedman <per@liedman.net>
2020-01-06 13:36:16 +0100

    Some cleanup/improvement re: highlighting parent objects but still not good enough

--- src/Highlight.svelte
+++ src/Highlight.svelte
@@ -57,15 +57,17 @@
       map.setFilter(`highlighted-${t}`, ['==', ['id'], '']))
 
     if (features.length > 0) {
-      const feature = features[0]
+      const feature = features.find(f => f.layer && f.layer.metadata)
       const highlightLayer = `highlighted-${feature.geometry.type}`
-      const filterExpr = feature.properties.parentId
-        ? ['get', 'parentId']
-        : ['id']
       const filterId = feature.properties.parentId
         ? feature.properties.parentId
         : feature.id
-      map.setFilter(highlightLayer, ['==', filterExpr, filterId])
+      const filterExpr = [
+        'any', 
+        ['==', ['id'], filterId], 
+        ['==', ['get', 'parentId'], filterId]
+      ]
+      map.setFilter(highlightLayer, filterExpr)
       highlighted = {
         type: 'Feature',
         properties: {
