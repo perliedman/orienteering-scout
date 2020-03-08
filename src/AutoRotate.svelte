5d43128ef86651aea3949c9e2b20e97241203ab3
Per Liedman <per@liedman.net>
2020-03-08 21:28:00 +0100

    Some debug info

--- src/AutoRotate.svelte
+++ src/AutoRotate.svelte
@@ -1,5 +1,6 @@
 <script>
   import { getContext, createEventDispatcher } from 'svelte'
+  import { readable } from 'svelte/store'
   import { mapboxgl, key } from './mapbox'
   import AutoRotateControl from './auto-rotate-control'
 
@@ -7,8 +8,43 @@
   const context = getContext(key)
   const map = context.getMap()
 
+  let enabled = false
+
+  function enable () {
+    enabled = true
+    dispatch('enable')
+  }
+
+  function disable () {
+    enabled = false
+    dispatch('disable')
+  }
+
+  function update (e) {
+    dispatch('bearing', e.bearing)
+  }
+
   const autoRotateControl = new AutoRotateControl()
-  autoRotateControl.on('bearing', e => dispatch('bearing', e.bearing))
-  autoRotateControl.on('disable', e => dispatch('disable'))
+  autoRotateControl.on('bearing', update)
+  autoRotateControl.on('enable', enable)
+  autoRotateControl.on('disable', disable)
   map.addControl(autoRotateControl)
+
+  const lastEvent = readable(null, function (set) {
+    autoRotateControl.on('bearing', event => set(event))
+  })
 </script>
+
+{#if enabled && $lastEvent}
+<div>
+  <pre>{JSON.stringify($lastEvent, null, 2)}</pre>
+</div>
+{/if}
+
+<style>
+  div {
+    position: absolute;
+    bottom: 16px;
+    right: 16px;
+  }
+</style>
